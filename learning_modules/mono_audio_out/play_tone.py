768e7e24d474d3e3cd008c92d6febbc9b2ad687c
MrPrattASH <101632496+MrPrattASH@users.noreply.github.com>
2022-08-24 15:51:08 +0200

    analog_audio
    upload analog audio code

--- learning_modules/mono_audio_out/play_tone.py
+++ learning_modules/mono_audio_out/play_tone.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-# Modified by Brogan Pratt for M4 board
+#Modified by Brogan Pratt for M4 board
 # SPDX-License-Identifier: MIT
 
 """CircuitPython Essentials Audio Out tone example"""
@@ -10,13 +10,15 @@ import board
 from audioio import AudioOut
 from audiocore import RawSample
 
-tone_volume = 0.1  # Increase this to increase the volume of the tone.
-frequency = 440  # Set this to the Hz of the tone you want to generate.
+frequency = 400  # Set this to the Hz of the tone you want to generate.
+
+# Generate one period of sine wav.
 length = 8000 // frequency
 sine_wave = array.array("H", [0] * length)
 for i in range(length):
-    sine_wave[i] = int((1 + math.sin(math.pi * 2 * i / length)) * tone_volume * (2 ** 15 - 1))
+    sine_wave[i] = int((1 + math.sin(math.pi * 2 * i / length)) * (2 ** 15 - 1))
 
+#init STEMMA Speaker
 audio = AudioOut(board.A0)
 sine_wave_sample = RawSample(sine_wave)
 
