1d3fbbe2884950a97197a4d8957ecb99b25f0fe3
MrPrattASH <101632496+MrPrattASH@users.noreply.github.com>
2022-10-06 13:14:56 +0200

    organizing
    renamed cheat sheets

--- RoboClaw_motor_controller/Rc_control_debug.py
+++ RoboClaw_motor_controller/Rc_control_debug.py
@@ -1,80 +1,130 @@
 import time
 import board
 import pwmio
+from digitalio import DigitalInOut, Direction, Pull
 from adafruit_motor import servo
 
-'''
+"""
 Button mode 1 (RC)
-Option 4 (Tank Style)
+Option 4 (Tank Style) - intended state to run this program
 Option 5 (Differential Steering Style)
-'''
+"""
 
-#init motors as pwm objects
+# init DC motors as pwm objects
 motor1 = pwmio.PWMOut(board.D2, frequency=50)
 motor2 = pwmio.PWMOut(board.D3, frequency=50)
 
-#init servo main arm up/down control
+# init servo main arm up/down control
 pwm = pwmio.PWMOut(board.D4, frequency=50)
 main_arm = servo.ContinuousServo(pwm)
-#+ is down, - is Up
-#8sec to top of arm, 7 sec down
+# + is down, - is Up
+# 8sec to top of arm, 7 sec down
 
-
-
-#servo gripper arms
-#d5 left
-#d6 right
+# servo gripper arms
+# d5 left
+# d6 right
 pwm1 = pwmio.PWMOut(board.D5, frequency=50)
 left_servo = servo.Servo(pwm1)
 pwm2 = pwmio.PWMOut(board.D6, frequency=50)
 right_servo = servo.Servo(pwm2)
+
+# buttons
+# D7 Yellow
+# D8 Red
+red_button = DigitalInOut(board.D8)
+yellow_button = DigitalInOut(board.D7)
+red_button.direction = Direction.INPUT
+yellow_button.direction = Direction.INPUT
+red_button.pull = Pull.UP
+yellow_button.pull = Pull.UP
+
+red_cur_state = False
+red_prev_state = False
+yellow_cur_state = False
+yellow_prev_state = False
+
 
 def servo_duty_cycle(pulse_ms, frequency=50):
     period_ms = 1.0 / frequency * 1000.0
     duty_cycle = int(pulse_ms / (period_ms / 65535.0))
     return duty_cycle
 
-#Define Min/Max/Stop commands
-min_speed = 1.0 #full forward
-max_speed = 2.0 #full backward
-stop = 1.5 #needs to be calibrated by motor.
+
+# Define Min/Max/Stop commands
+min_speed = 1.0  # full forward
+max_speed = 2.0  # full backward
+stop = 1.5  # needs to be calibrated by motor.
+
 
 def motor_test():
-    #Main motor move forward, stop, backwards, stop
+    SLEEP_DURATION = 2
+    # Main motor move forward, stop, backwards, stop
     motor1.duty_cycle = servo_duty_cycle(min_speed)
     motor2.duty_cycle = servo_duty_cycle(min_speed)
     print("Full forward")
-    time.sleep(2)
+    time.sleep(SLEEP_DURATION)
 
     motor1.duty_cycle = servo_duty_cycle(stop)
     motor2.duty_cycle = servo_duty_cycle(stop)
     print("stop")
-    time.sleep(2)
+    time.sleep(SLEEP_DURATION)
 
     motor1.duty_cycle = servo_duty_cycle(max_speed)
     motor2.duty_cycle = servo_duty_cycle(max_speed)
     print("Full backward")
-    time.sleep(2)
+    time.sleep(SLEEP_DURATION)
 
     motor1.duty_cycle = servo_duty_cycle(stop)
     motor2.duty_cycle = servo_duty_cycle(stop)
     print("stop")
-    time.sleep(2)
 
-def arm_full_up():
-    main_arm.throttle = -1
-    time.sleep(12.2)
 
-def arm_full_return():
-    main_arm.throttle = 1
-    time.sleep(12.2)
+def arm_full_up(cur_time, start_time):
+    goal_time = 12.2
+    if not cur_time >= start_time + goal_time:
+        main_arm.throttle = -1
+        return True
+    else:
+        main_arm.throttle = 0
+        return False
+
+
+def arm_full_return(cur_time, start_time):
+    goal_time = 12.2
+    if not cur_time >= start_time + goal_time:
+        main_arm.throttle = 1
+        return True
+    else:
+        main_arm.throttle = 0
+        return False
+
 
 def grab():
     pass
-#arm_full_up()
-#arm_full_return()
+
+arm_up = False
+arm_up_goal_time = 12.2
 
 while True:
-    time.sleep(5)
-    motor_test()
+    now = time.monotonic()
+    # button logic
+    yellow_cur_state = yellow_button.value
+    red_cur_state = red_button.value
+
+    if yellow_cur_state != yellow_prev_state:
+        #push button, begin start time, start arm up
+        if not yellow_cur_state: #button is down
+            if not arm_full_up
+
+
+    if red_cur_state != red_prev_state:
+        if not red_cur_state:
+            red_start_time = now
+
+    yellow_prev_state = yellow_cur_state
+    red_prev_state = red_cur_state
+
+    #move movement arm
+    arm_full_up(now, yellow_start_time)
+
 
--- learning_modules/ping_sensor/sonarbit.py
+++ learning_modules/ping_sensor/sonarbit.py
@@ -1,7 +1,7 @@
 
 
 """ Sonarbit Distance Program
-Credit to student Z. for assistance in making this code more efficient. 
+Written by Brogan Pratt & Improvements made by u/mirusz9
 """
 
 import time
@@ -9,19 +9,14 @@ import board
 import digitalio
 from math import floor
 
-def sonar(pin, prev_distance = 570, sleep = True):
-    ''' Get a ping reading from 5cm>568cm from the Elecfreaks Pin Sensor. Trigger & Echo are on the 'same pin'. 
-    pin = accepts pin argument with the ping sensor attached
-    prev_distance: accepts an int between 0>568. intended to catch false 568 high readings if <5cm to sensor
-        default value is only given for initial run, after this initial call you should input a previous distance
-    sleep = Accepts boolean. Intended to introduce a 0.05ms sleep inbetween sensor readings, else program crashes from too many calls to the sensor. 
+def sonar(pin, prev_distance = 570):
+    ''' Get a ping reading from 5cm>568cm from the Elecfreaks Ping Sensor. Trigger & Echo are on the 'same pin'.
+    pin = board.pin with the ping sensor attached
+    prev_distance: accepts an int between 0>568. This sensor will output false 568 high readings if the object is <5cm to sensor.
+        The default value is only given for the initial run, after this initial call you should input a previous distance
     returns: Distance in cm between 0 & 568
-    
-
-    Note: Introduce a 0.05sec delay when taking sensor readings in CircuitPython, else the code will stop parsing. 
-    
     '''
-    
+
     usleep = lambda x: x/1000000
 
     pin.direction = digitalio.Direction.OUTPUT
@@ -35,19 +30,19 @@ def sonar(pin, prev_distance = 570, sleep = True):
 
     #recieve echo signal
     pin.direction = digitalio.Direction.INPUT
-    original_time = time.monotonic_ns() 
+    original_time = time.monotonic_ns()
 
     while pin.value == 0:
         start_time = time.monotonic_ns()
         #this stops an infinite loop sometimes caused by rapid movements in front of the sensor. I am unsure of why
-        #this happens occassionally, but this "safety check" stops this from happening
+        #this happens, but this "safety check" breaks the infinite loop
         if (start_time - original_time > 50000000):
             break
 
     while pin.value == 1:
         end_time = time.monotonic_ns()
 
-    #If an object is too close, start_time will never be assigned because pin.value will never equal 0, causing a variable assignment error & crash. 
+    #If an object is too close, start_time will never be assigned because pin.value will never equal 0, causing a variable assignment error & crash.
     try:
         # Distance is defined as time/2 (there and back) * speed of sound 34000 cm/s
         distance = floor((end_time - start_time) * 34000/2/1000000000)
@@ -55,22 +50,21 @@ def sonar(pin, prev_distance = 570, sleep = True):
         distance = 0
 
     #check if user has entered a previous distance
-    if prev_distance >= 570:
+    if prev_distance = 570:
         pass
     else:
-        #if our previous distance was close, and suddenly we register a VERY FAR object, we clearly are reading a false high 568cm, return the previously read value. 
+        #if our previous distance was close, and suddenly we register a VERY FAR object, we clearly are reading a false high 568cm, return the previously read value.
         if prev_distance < 15 and distance > 150:
-            distance = prev_dist
-    
-    #you may take this out if necesssary and you are doing other time based functions on your code. If you are not, leave this in otherwise you will send out signals too quickly for what the ping sensor can send/recieve from. 
-    if sleep:
-        time.sleep(0.05)
+            distance = prev_distance
+
     return distance
 
-
+#init a prev_distance value for sonar func
+prev_distance = 300
 pin = digitalio.DigitalInOut(board.D2)
 while True:
-    distance = sonar(pin)
+    distance = sonar(pin, prev_distance)
     print("the object is " + str(distance) + " cm away")
-    time.sleep(0.05)
+    prev_distance = distance
+
 
